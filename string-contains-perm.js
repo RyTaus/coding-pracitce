1feeef0f427ca128a45302380d38437132ceb31c
RyTaus <rtaus1@lion.lmu.edu>
2017-09-08 17:08:32 -0700

    added a strings-contains-perm solution (CtCI pg. 70)

---
+++ string-contains-perm.js
@@ -0,0 +1,30 @@
+const isPerm = (test, string) => {
+  if (test.length !== string.length) {
+    return false
+  }
+  if (test === '' && string === '') {
+    return true;
+  }
+  if (string.includes(test[0])) {
+    return isPerm(test.substring(1), string.replace(test[0], ''))
+  }
+  return false;
+}
+
+const stringContainsPerm = (small, large) => {
+  let potentialAnswers = []
+  const solutions = []
+  for (let i = 0; i < large.length; i ++) {
+    currLetter = large[i];
+    potentialAnswers.push('')
+    potentialAnswers = potentialAnswers.map(s => `${s}${large[i]}`)
+    if (i >= small.length - 1) {
+      const potentialSolution = potentialAnswers.shift()
+      if (isPerm(potentialSolution, small)) {
+        solutions.push(potentialSolution);
+      }
+    }
+  }
+
+  return solutions;
+}
